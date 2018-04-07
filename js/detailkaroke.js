0b552b96788ad46bfd600bf16ff58e618b55c05f
Ravindran Raghu <sairavi23@Ravindrans-MacBook-Pro.local>
2018-04-07 10:57:41 -0400

    Removed js file which is merged in to detail.js

--- js/detailkaroke.js
+++
@@ -1,7 +0,0 @@
-function detailKarokeJson (json)
-{
-    var karokeHtml = "";
-    karokeHtml += '<div>source: '+ json.source  +'</div>'
-    karokeHtml += '<div><span>scale: '+ json.reference.scale  +'</span> <audio src="'+json.reference.url+'"></audio> </div>';
-    return karokeHtml;
-}
